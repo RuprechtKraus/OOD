58fca6be228ccc689bf2b37066edb21a33bcce62
Ruprecht <zombiegooood@gmail.com>
2022-09-26 06:23:33 +0300

    Удалены старые проекты

--- Lab2/WeatherStation/Display.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "IObserver.h"
-#include "WeatherData.h"
-
-class Display : public IObserver<WeatherInfo>
-{
-private:
-	void Update(const WeatherInfo& data) const noexcept override;
-};
--- Lab2/WeatherStation/IObserver.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-template <typename T>
-class IObserver
-{
-public:
-	virtual void Update(const T& data) const = 0;
-	virtual ~IObserver() = default;
-};
