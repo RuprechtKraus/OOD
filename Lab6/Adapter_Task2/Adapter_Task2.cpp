dddc541d3d3e72053b688580192fe47cf04f643e
Ruprecht Kraus <zombiegooood@gmail.com>
2022-11-12 19:38:15 +0300

    Второе задание

---
+++ Lab6/Adapter_Task2/Adapter_Task2.cpp
@@ -0,0 +1,48 @@
+﻿#include "ModernGraphicsLib/ModernGraphicsLib.h"
+#include "ModernGraphicsLibAdapter/ModernGraphicsRendererAdapter.h"
+#include "ShapeDrawingLib/ShapeDrawingLib.h"
+#include <iostream>
+
+// Пространство имен приложения (доступно для модификации)
+namespace app
+{
+void PaintPicture(shape_drawing_lib::CanvasPainter& painter)
+{
+	using namespace shape_drawing_lib;
+
+	Triangle triangle({ 10, 15 }, { 100, 200 }, { 150, 250 });
+	Rectangle rectangle({ 30, 40 }, 18, 24);
+
+	painter.Draw(triangle);
+	painter.Draw(rectangle);
+}
+
+void PaintPictureOnCanvas()
+{
+	graphics_lib::Canvas simpleCanvas;
+	shape_drawing_lib::CanvasPainter painter(simpleCanvas);
+	PaintPicture(painter);
+}
+
+void PaintPictureOnModernGraphicsRenderer()
+{
+	ModernGraphicsRendererAdapter rendererAdapter(std::cout);
+	shape_drawing_lib::CanvasPainter painter(rendererAdapter);
+	PaintPicture(painter);
+}
+} // namespace app
+
+int main()
+{
+	std::cout << "Should we use new API (y)?";
+	std::string userInput;
+
+	if (std::getline(std::cin, userInput) && (userInput == "y" || userInput == "Y"))
+	{
+		app::PaintPictureOnModernGraphicsRenderer();
+	}
+	else
+	{
+		app::PaintPictureOnCanvas();
+	}
+}
---
+++ Lab6/Adapter_Task2/ModernGraphicsLibAdapter/ModernGraphicsRendererAdapter.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "GraphicsLib/GraphicsLib.h"
+#include "ModernGraphicsLib/ModernGraphicsLib.h"
+
+namespace mgl = modern_graphics_lib;
+
+class ModernGraphicsRendererAdapter : public graphics_lib::ICanvas, public mgl::ModernGraphicsRenderer
+{
+public:
+	ModernGraphicsRendererAdapter(std::ostream& out)
+		: ModernGraphicsRenderer(out)
+		, m_penCoordinates(0, 0)
+		, m_drawing(false)
+	{
+	}
+
+	void MoveTo(int x, int y) override
+	{
+		m_penCoordinates.x = x;
+		m_penCoordinates.y = y;
+	}
+
+	void LineTo(int x, int y) override
+	{
+		if (!m_drawing)
+		{
+			BeginDraw();
+			m_drawing = true;
+		}
+
+		DrawLine(m_penCoordinates, { x, y });
+		MoveTo(x, y);
+	}
+
+private:
+	mgl::Point m_penCoordinates;
+	bool m_drawing;
+};
