0799deb814e1a7dc4c4d48cbc5a769c26209906d
Ruprecht Kraus <zombiegooood@gmail.com>
2022-11-13 19:17:49 +0300

    Сделано третье задание

---
+++ Lab6/Adapter_Task3/ModernGraphicsLib/ModernGraphicsLib.cpp
@@ -0,0 +1,55 @@
+#include "ModernGraphicsLib.h"
+#include <format>
+#include <iomanip>
+
+namespace modern_graphics_lib
+{
+ModernGraphicsRenderer::ModernGraphicsRenderer(std::ostream& strm)
+	: m_out(strm)
+{
+}
+
+ModernGraphicsRenderer::~ModernGraphicsRenderer()
+{
+	if (m_drawing) // «авершаем рисование, если оно было начато
+	{
+		EndDraw();
+	}
+}
+
+void ModernGraphicsRenderer::BeginDraw()
+{
+	if (m_drawing)
+	{
+		throw std::logic_error("Drawing has already begun");
+	}
+	m_out << "<draw>" << std::endl;
+	m_drawing = true;
+}
+
+void ModernGraphicsRenderer::DrawLine(const Point& start, const Point& end, const RGBAColor& color)
+{
+	if (!m_drawing)
+	{
+		throw std::logic_error("DrawLine is allowed between BeginDraw()/EndDraw() only");
+	}
+	m_out << std::format(
+		R"(  <line fromX="{}" fromY="{}" toX="{}" toY="{}">)", start.x, start.y, end.x, end.y)
+		  << '\n'
+		  << std::format(
+				 R"(    <color r="{:.2f}" g="{:.2f}" b="{:.2f}" a="{:.2f}" />)", color.r, color.g, color.b, color.a)
+		  << '\n'
+		  << "  </line>"
+		  << std::endl;
+}
+
+void ModernGraphicsRenderer::EndDraw()
+{
+	if (!m_drawing)
+	{
+		throw std::logic_error("Drawing has not been started");
+	}
+	m_out << "</draw>" << std::endl;
+	m_drawing = false;
+}
+} // namespace modern_graphics_lib
---
+++ Lab6/Adapter_Task3/ModernGraphicsLibAdapter/ModernGraphicsRendererAdapter.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "GraphicsLib/GraphicsLib.h"
+#include "ModernGraphicsLib/ModernGraphicsLib.h"
+
+namespace mgl = modern_graphics_lib;
+
+class ModernGraphicsRendererAdapter : public graphics_lib::ICanvas
+	, public mgl::ModernGraphicsRenderer
+{
+public:
+	ModernGraphicsRendererAdapter(std::ostream& out)
+		: ModernGraphicsRenderer(out)
+		, m_penCoordinates(0, 0)
+		, m_color({ 0, 0, 0, 0 })
+		, m_drawing(false)
+	{
+	}
+
+	void SetColor(uint32_t rgbColor) override
+	{
+		float r = ((rgbColor & 0xff0000) >> 16) / 255.0f,
+			  g = ((rgbColor & 0x00ff00) >> 8) / 255.0f,
+			  b = (rgbColor & 0x0000ff) / 255.0f,
+			  a = 1;
+		m_color = { r, g, b, a };
+	}
+
+	void MoveTo(int x, int y) override
+	{
+		m_penCoordinates.x = x;
+		m_penCoordinates.y = y;
+	}
+
+	void LineTo(int x, int y) override
+	{
+		if (!m_drawing)
+		{
+			BeginDraw();
+			m_drawing = true;
+		}
+
+		DrawLine(m_penCoordinates, { x, y }, m_color);
+		MoveTo(x, y);
+	}
+
+private:
+	mgl::Point m_penCoordinates;
+	modern_graphics_lib::RGBAColor m_color;
+	bool m_drawing;
+};
